a7085f594cfd5de9e207490ecf4f7a849f84bae4
SandiS2703 <sandis2703@gmail.com>
2024-05-11 02:19:35 +0700

    Update Progress Home Data Statis

--- resources/views/component/guest/head-guest.blade.php
+++ resources/views/component/guest/head-guest.blade.php
@@ -4,5 +4,11 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/style-guest/style.css">
+    <link rel="stylesheet" href="/_settings/_base.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
