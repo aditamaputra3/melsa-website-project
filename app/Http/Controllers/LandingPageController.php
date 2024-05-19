263c44c17be2aaa68188ae9004f70389cd4b1037
SandiS2703 <sandis2703@gmail.com>
2024-05-19 12:06:10 +0700

    update data dinamis keseluruhan view produk

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produk;
 
 class LandingPageController extends Controller
 {
-    public function index(){
-        // 	$Praktikum = Praktikum::count();
-        // $Asisten = AsistenLab::count();
-        // $Pengajuan= PengajuanJadwal::where('status_pengajuan', '=', 'menunggu')->count();
-        // $Koordinator = Dosen::count();
-        
-        return view('view-guest.view-home');
-    
+    public function index()
+    {
+        $ProdukKue = Produk::where('jenis_produk', 'kue')->get();
+        $ProdukCatering = Produk::where('jenis_produk', 'catering')->get();
+
+        return view('view-guest.view-home', compact('ProdukKue', 'ProdukCatering'));
+
         // return view('dashboard.index', compact('Praktikum', 'Asisten', 'Pengajuan','Koordinator'));
-        }
+    }
 }
--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Produk;
+use Error;
 
 class MenuController extends Controller
 {
@@ -13,5 +14,19 @@ class MenuController extends Controller
         $ProdukCatering = Produk::where('jenis_produk', 'catering')->get();
 
         return view('view-guest.view-menu', compact('ProdukKue', 'ProdukCatering'));
+    }
+
+    public function getDetail($id)
+    {
+        $related = null;
+        $data = Produk::findOrFail($id);
+        if ($data['jenis_produk'] === 'catering') {
+            $related = Produk::where('jenis_produk', 'catering')->get();
+        }
+        if ($data['jenis_produk'] === 'kue') {
+            $related = Produk::where('jenis_produk', 'kue')->get();
+        }
+
+        return view('view-guest.view-menu-detail', compact('data', 'related'));
     }
 }
