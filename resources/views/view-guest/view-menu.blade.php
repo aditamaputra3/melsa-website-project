21b9d16bb9079f5457dee12bb5b3769d2e23a020
SandiS2703 <sandis2703@gmail.com>
2024-05-24 05:47:58 +0700

    Fitur kategori 💀💀

--- resources/views/view-guest/view-menu.blade.php
+++ resources/views/view-guest/view-menu.blade.php
@@ -8,37 +8,41 @@
             </div>
             <a class="menu-title-inactive">Melsa Kue</a>
         </div>
-        <div class="menu-content-container catering-content-container">
-            <div class="menu-controller-container mb-5">
-                <div class="dropdown">
-                    <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Kategori Produk
-                    </button>
-                    <ul class="dropdown-menu">
-                        @foreach ($Kategori as $jenis)
-                            <li><a class="dropdown-item" href="">{{ $jenis->nama_kategori }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-                <div class="dropdown">
-                    <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Urutkan
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
-                <form class="input-group search-container">
-                    <input class="form-search form-control fst-italic" type="search" aria-describedby="search-form"
-                        placeholder="Ketik nama produk yang anda cari...">
-                    <span><button class="input-group-text btn-search" type="submit" id="search-form">Cari</button>
-                    </span>
-                </form>
+        <div class="menu-controller-container mb-5">
+            <div class="dropdown">
+                <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Kategori Produk
+                </button>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a class="dropdown-item" href="/menu">Tampilkan semua</a>
+                    </li>
+                    @foreach ($Kategori as $jenis)
+                        <li>
+                            <a class="dropdown-item" href="/menu?id={{ $jenis->id }}">{{ $jenis->nama_kategori }}</a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
-            <div class="menu-container mb-5 p-2 catering-menu">
+            <div class="dropdown">
+                <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Urutkan
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+            <form class="input-group search-container">
+                <input class="form-search form-control fst-italic" type="search" aria-describedby="search-form"
+                    placeholder="Ketik nama produk yang anda cari..." id="search-catering">
+                <span><button class="input-group-text btn-search" type="submit" id="search-form">Cari</button>
+                </span>
+            </form>
+        </div>
+        <div class="menu-content-container catering-content-container active-menu">
+            <div class="menu-container mb-5 p-2 catering-menu" id="cateringMenu">
                 @foreach ($ProdukCatering as $produk)
                     <a class="menu-card" href="{{ route('detail-produk', ['id' => $produk->id]) }}">
                         <div class="menu-card-img ratio ratio-4x3">
@@ -47,47 +51,14 @@
                         <div class="menu-card-body">
                             <h5 class="menu-title fw-bold">{{ $produk->nama_produk }}</h5>
                             <p class="menu-desc">{{ $produk->deskripsi_produk }}</p>
-                            <h5 class="menu-price fw-bold">Rp {{ number_format($produk->harga_produk, 0, ',', '.') }}</h5>
+                            <h5 class="menu-price fw-bold">Rp {{ number_format($produk->harga_produk, 0, ',', '.') }}
+                            </h5>
                         </div>
                     </a>
                 @endforeach
             </div>
         </div>
-        <div class="menu-content-container cake-content-container">
-            <div class="menu-controller-container mb-5">
-                <div class="dropdown">
-                    <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Kategori Produk
-                    </button>
-                    <ul class="dropdown-menu">
-                        @foreach ($Kategori as $jenis)
-                            <li><a class="dropdown-item kategori-produk">{{ $jenis->nama_kategori }}</a>
-                            </li>
-                        @endforeach
-                        <script>
-                            $(".kategori-produk").on('click', function() {
-                                
-                            })
-                        </script>
-                    </ul>
-                </div>
-                <div class="dropdown">
-                    <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Urutkan
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
-                <form class="input-group search-container">
-                    <input class="form-search form-control fst-italic" type="search" aria-describedby="search-form"
-                        placeholder="Ketik nama produk yang anda cari...">
-                    <span><button class="input-group-text btn-search" type="submit" id="search-form">Cari</button>
-                    </span>
-                </form>
-            </div>
+        <div class="menu-content-container cake-content-container inactive-menu">
             <div class="menu-container mb-5 p-2 cake-menu">
                 @foreach ($ProdukKue as $produk)
                     <a class="menu-card" href="{{ route('detail-produk', ['id' => $produk->id]) }}">
@@ -97,7 +68,8 @@
                         <div class="menu-card-body">
                             <h5 class="menu-title fw-bold">{{ $produk->nama_produk }}</h5>
                             <p class="menu-desc">{{ $produk->deskripsi_produk }}</p>
-                            <h5 class="menu-price fw-bold">Rp {{ number_format($produk->harga_produk, 0, ',', '.') }}</h5>
+                            <h5 class="menu-price fw-bold">Rp {{ number_format($produk->harga_produk, 0, ',', '.') }}
+                            </h5>
                         </div>
                     </a>
                 @endforeach
