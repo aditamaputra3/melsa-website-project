27c5b0e0a89ea2e293843efa6f5f43065b9f4fc8
SandiS2703 <sandis2703@gmail.com>
2024-05-25 22:22:29 +0700

    Fitur Sort dan Search Beres 😊

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -8,20 +8,45 @@ use App\Models\Produk;
 use Error;
 use Mockery\Undefined;
 
+use function Laravel\Prompts\alert;
 use function PHPUnit\Framework\isEmpty;
 
 class MenuController extends Controller
 {
     public function index($id = null)
     {
+        if (isset($_GET['priceAsc'])) {
+            $ProdukKue = Produk::where('jenis_produk', 'kue')->orderBy('harga_produk', 'ASC')->get();
+            $ProdukCatering = Produk::where('jenis_produk', 'catering')->orderBy('harga_produk', 'ASC')->get();
+            $Kategori = Kategori::all();
+        } elseif (isset($_GET['priceDesc'])) {
+            $ProdukKue = Produk::where('jenis_produk', 'kue')->orderBy('harga_produk', 'DESC')->get();
+            $ProdukCatering = Produk::where('jenis_produk', 'catering')->orderBy('harga_produk', 'DESC')->get();
+            $Kategori = Kategori::all();
+        } elseif (isset($_GET['nameAsc'])) {
+            $ProdukKue = Produk::where('jenis_produk', 'kue')->orderBy('nama_produk', 'ASC')->get();
+            $ProdukCatering = Produk::where('jenis_produk', 'catering')->orderBy('nama_produk', 'ASC')->get();
+            $Kategori = Kategori::all();
+        } elseif (isset($_GET['search'])) {
+            if ($_GET['search'] != null) {
+                $search = $_GET['search'];
+                $ProdukKue = Produk::where('jenis_produk', 'kue')->where('nama_produk', 'LIKE', "%{$search}%")->orwhere('harga_produk', 'LIKE', "%{$search}%")->get();
+                $ProdukCatering = Produk::where('jenis_produk', 'catering')->where('nama_produk', 'LIKE', "%{$search}%")->orwhere('harga_produk', 'LIKE', "%{$search}%")->get();
+                $Kategori = Kategori::all();
+            } else {
+                $ProdukKue = Produk::where('jenis_produk', 'kue')->get();
+                $ProdukCatering = Produk::where('jenis_produk', 'catering')->get();
+                $Kategori = Kategori::all();
+            }
+        } else {
+            $ProdukKue = Produk::where('jenis_produk', 'kue')->get();
+            $ProdukCatering = Produk::where('jenis_produk', 'catering')->get();
+            $Kategori = Kategori::all();
+        }
         if (isset($_GET['id'])) {
             $id = $_GET['id'];
             $ProdukKue = Produk::where('id_kategori', $id)->where('jenis_produk', 'kue')->get();
             $ProdukCatering = Produk::where('id_kategori', $id)->where('jenis_produk', 'catering')->get();
-            $Kategori = Kategori::all();
-        } else {
-            $ProdukKue = Produk::where('jenis_produk', 'kue')->get();
-            $ProdukCatering = Produk::where('jenis_produk', 'catering')->get();
             $Kategori = Kategori::all();
         }
 
@@ -40,5 +65,12 @@ class MenuController extends Controller
         }
 
         return view('view-guest.view-menu-detail', compact('data', 'related'));
+    }
+
+    public function sortPrice()
+    {
+        $ProdukKue = Produk::where('jenis_produk', 'kue')->orderBy('harga_produk', 'desc')->get();
+        $ProdukCatering = Produk::where('jenis_produk', 'catering')->orderBy('harga_produk', 'desc')->get();
+        return compact('ProdukKue', 'ProdukCatering');
     }
 }
--- resources/views/view-guest/view-menu.blade.php
+++ resources/views/view-guest/view-menu.blade.php
@@ -36,14 +36,15 @@
                     Urutkan
                 </button>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><a class="dropdown-item" href="/menu">Jangan Urutkan</a></li>
+                    <li><a class="dropdown-item" href="/menu?priceAsc=1">Harga Terendah</a></li>
+                    <li><a class="dropdown-item" href="/menu?priceDesc=1">Harga Tertinggi</a></li>
+                    <li><a class="dropdown-item" href="/menu?nameAsc=1">Nama Menu</a></li>
                 </ul>
             </div>
-            <form class="input-group search-container">
+            <form class="input-group search-container" action="/menu" method="GET">
                 <input class="form-search form-control fst-italic" type="search" aria-describedby="search-form"
-                    placeholder="Ketik nama produk yang anda cari..." id="search-catering">
+                    placeholder="Ketik nama produk yang anda cari..." id="search" name="search">
                 <span><button class="input-group-text btn-search" type="submit" id="search-form">Cari</button>
                 </span>
             </form>
@@ -96,6 +97,58 @@
                     <div class="p-5">
                         <H1 class="my-5">NOT FOUND</H1>
                     </div>
+                </div>
+            </div>
+        @elseif(count($ProdukCatering) === 0 && count($ProdukKue) === 0)
+            <div class="menu-content-container cake-content-container active-menu">
+                <div class="menu-container mb-5 p-2 cake-menu">
+                    <div class="p-5">
+                        <H1 class="my-5">NOT FOUND</H1>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-content-container catering-content-container inactive-menu">
+                <div class="menu-container mb-5 p-2 catering-menu" id="cateringMenu">
+                    <div class="p-5">
+                        <H1 class="my-5">NOT FOUND</H1>
+                    </div>
+                </div>
+            </div>
+        @else
+            <div class="menu-content-container catering-content-container active-menu">
+                <div class="menu-container mb-5 p-2 catering-menu" id="cateringMenu">
+                    @foreach ($ProdukCatering as $produk)
+                        <a class="menu-card" href="{{ route('detail-produk', ['id' => $produk->id]) }}">
+                            <div class="menu-card-img ratio ratio-4x3">
+                                <img src="{{ asset('storage/' . $produk->foto_produk) }}"
+                                    alt="{{ $produk->nama_produk }}">
+                            </div>
+                            <div class="menu-card-body">
+                                <h5 class="menu-title fw-bold">{{ $produk->nama_produk }}</h5>
+                                <p class="menu-desc">{{ $produk->deskripsi_produk }}</p>
+                                <h5 class="menu-price fw-bold">Rp {{ number_format($produk->harga_produk, 0, ',', '.') }}
+                                </h5>
+                            </div>
+                        </a>
+                    @endforeach
+                </div>
+            </div>
+            <div class="menu-content-container cake-content-container inactive-menu">
+                <div class="menu-container mb-5 p-2 cake-menu">
+                    @foreach ($ProdukKue as $produk)
+                        <a class="menu-card" href="{{ route('detail-produk', ['id' => $produk->id]) }}">
+                            <div class="menu-card-img ratio ratio-4x3">
+                                <img src="{{ asset('storage/' . $produk->foto_produk) }}"
+                                    alt="{{ $produk->nama_produk }}">
+                            </div>
+                            <div class="menu-card-body">
+                                <h5 class="menu-title fw-bold">{{ $produk->nama_produk }}</h5>
+                                <p class="menu-desc">{{ $produk->deskripsi_produk }}</p>
+                                <h5 class="menu-price fw-bold">Rp {{ number_format($produk->harga_produk, 0, ',', '.') }}
+                                </h5>
+                            </div>
+                        </a>
+                    @endforeach
                 </div>
             </div>
         @endif
