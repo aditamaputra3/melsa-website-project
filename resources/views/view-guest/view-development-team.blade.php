b185c6cb0032d0b8ad021a9309de01be58eae13b
SandiS2703 <sandis2703@gmail.com>
2024-06-27 12:18:13 +0700

    Add Halaman Tim Pengembang

---
+++ resources/views/view-guest/view-development-team.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/style-guest/style.css">
+    <title>Tim Pengembang - Sistem Informasi Itenas</title>
+</head>
+
+<body>
+    <div class="text-center mt-4">
+        <h1>Tim Pengembang - Sistem Informasi ITENAS</h1>
+    </div>
+    <div class="d-flex justify-content-center p-5">
+        <div class="team-container">
+            <div class="img-team-container-wrap ">
+                <img src="/assets/img/tim-pengembang/Pa asep.jpeg" class="d-block w-100 h-100 mb-2" alt="...">
+                <h5 class="text-center">Asep Rizal Nurjaman</h5>
+                <p class="text-center">Pembimbing Project</p>
+            </div>
+            <div class="img-team-container-wrap ">
+                <img src="/assets/img/tim-pengembang/mona.jpeg" class="d-block w-100 h-100 mb-2" alt="...">
+                <h5 class="text-center">Monalisa Putri</h5>
+                <p class="text-center">UI/UX Designer</p>
+            </div>
+            <div class="img-team-container-wrap">
+                <img src="/assets/img/tim-pengembang/sandi.jpeg" class="d-block w-100 h-100 mb-2" alt="...">
+                <h5 class="text-center">Sandi Sinaga</h5>
+                <p class="text-center">Frontend Developer</p>
+            </div>
+            <div class="img-team-container-wrap">
+                <img src="/assets/img/tim-pengembang/dhea.jpeg" class="d-block w-100 h-100 mb-2" alt="...">
+                <h5 class="text-center">Dhea Halimatu Sa'diah</h5>
+                <p class="text-center">System Analyst</p>
+            </div>
+            <div class="img-team-container-wrap">
+                <img src="/assets/img/tim-pengembang/adi.jpeg" class="d-block w-100 h-100 mb-2" alt="...">
+                <h5 class="text-center">Aditama Putra</h5>
+                <p class="text-center">Backend Developer</p>
+            </div>
+            <div class="img-team-container-wrap">
+                <img src="/assets/img/tim-pengembang/balqis.jpeg" class="d-block w-100 h-100 mb-2" alt="...">
+                <h5 class="text-center">Balqis Azizah Pohan</h5>
+                <p class="text-center">Technical Writer</p>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
+</body>
+{{-- <div class="d-flex justify-content-center pt-5">
+    <a class="button-back-melsa" href="/">Back To Melsa</a>
+</div>
+<div class="w-100 h-100 d-flex justify-content-center pt-5">
+    <div id="carouselExampleCaptions" class="carousel slide w-25">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
+                aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4"
+                aria-label="Slide 5"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item img-team-container active">
+                <img src="/assets/img/tim-pengembang/mona.jpeg" class="d-block w-100 h-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-center">Monalisa Putri</h5>
+                    <p class="text-center">UI/UX Designer</p>
+                </div>
+            </div>
+            <div class="carousel-item img-team-container">
+                <img src="/assets/img/tim-pengembang/sandi.jpeg" class="d-block w-100 h-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-center">Sandi Sinaga</h5>
+                    <p class="text-center">Frontend Developer</p>
+                </div>
+            </div>
+            <div class="carousel-item img-team-container">
+                <img src="/assets/img/tim-pengembang/dhea.jpeg" class="d-block w-100 h-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-center">Dhea Halimatu Sa'diah</h5>
+                    <p class="text-center">System Analyst</p>
+                </div>
+            </div>
+            <div class="carousel-item img-team-container">
+                <img src="/assets/img/tim-pengembang/adi.jpeg" class="d-block w-100 h-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-center">Aditama Putra</h5>
+                    <p class="text-center">Backend Developer</p>
+                </div>
+            </div>
+            <div class="carousel-item img-team-container">
+                <img src="/assets/img/tim-pengembang/balqis.jpeg" class="d-block w-100 h-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-center">Balqis Azizah Pohan</h5>
+                    <p class="text-center">Technical Writer</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div> --}}
+
+</html>
